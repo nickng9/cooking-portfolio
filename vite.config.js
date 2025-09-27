ce1a47e57293213ac7d3b06e3a77b92f4e795b9d
Nicholas Ng <nicholas.m.ng@gmail.com>
2025-09-27 00:04:53 -0400

    updates for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// If deploying to GitHub Pages, set base to '/<repo-name>/'.
-// Example: base: '/nicholas-portfolio/'
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GH_PAGES ? '/nicholas-portfolio/' : '/'
+  base: "/cooking-portfolio/",
 });
